ce8c301e40b100dde421d0317dbb61a29be1aa5e
LiaoZiyang <lzy08238013@163.com>
2025-04-13 19:58:57 +0800

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+/**
+ * ClassName: ${NAME}
+ * Package: ${PACKAGE_NAME}
+ * Description:
+ *
+ * @Author:廖子杨
+ * @Create: 2025/4/13 - 19:54
+ * @Version: v 1.0
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
