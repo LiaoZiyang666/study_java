704691ba09e4951593c6283d43476340c6458884
LiaoZiyang <lzy08238013@163.com>
2025-04-13 20:01:36 +0800

    第一次提交，测试是否成功

---
+++ src/test/Hello.java
@@ -0,0 +1,16 @@
+package test;
+
+/**
+ * ClassName: Hello
+ * Package: test
+ * Description:
+ *
+ * @Author:廖子杨
+ * @Create: 2025/4/13 - 19:59
+ * @Version: v 1.0
+ */
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello World!!!");
+    }
+}
