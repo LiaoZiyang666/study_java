dc8f6e5983ef45be2badcf29bffb327d2caae1cd
LiaoZiyang <lzy08238013@163.com>
2025-04-13 20:19:44 +0800

    第5次提交，测试是否成功

---
+++ src/test/Test04.java
@@ -0,0 +1,16 @@
+package test;
+
+/**
+ * ClassName: Test04
+ * Package: test
+ * Description:
+ *
+ * @author 廖子杨
+ * CreateTime: 2025/4/13 - 20:19
+ * Version - v 1.0
+ */
+public class Test04 {
+    public static void main(String[] args) {
+        System.out.println("test");
+    }
+}
