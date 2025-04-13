eb68abcae773ebb3395f7086416cc92ff5d832a5
LiaoZiyang <lzy08238013@163.com>
2025-04-13 20:18:03 +0800

    第4次提交，测试是否成功

--- src/test/Test02.java
+++ src/test/Test02.java
@@ -11,6 +11,6 @@ package test;
 
 public class Test02 {
     public static void main(String[] args) {
-        System.out.println("测试模板");
+        
     }
 }
---
+++ src/test/Test03.java
@@ -0,0 +1,16 @@
+package test;
+
+/**
+ * ClassName: Test03
+ * Package: test
+ * Description:
+ *
+ * @author : 廖子杨
+ * Create : 2025/4/13 - 20:17
+ * Version : v 1.0
+ */
+public class Test03 {
+    public static void main(String[] args) {
+        System.out.println("test03");
+    }
+}
