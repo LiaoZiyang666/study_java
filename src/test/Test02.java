5d5103ce41dd7d8b02aad13aa6fdc35e0749bbbd
LiaoZiyang <lzy08238013@163.com>
2025-04-13 20:13:59 +0800

    第三次提交，测试是否成功

---
+++ src/test/Test02.java
@@ -0,0 +1,16 @@
+package test;
+
+/**
+ * ClassName: Test02
+ * Package: test
+ * Description:
+ *
+ * @author:廖子杨 Create: 2025/4/13 - 20:13
+ * Version: v 1.0
+ */
+
+public class Test02 {
+    public static void main(String[] args) {
+        System.out.println("测试模板");
+    }
+}
