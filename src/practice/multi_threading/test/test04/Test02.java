995a99cf549b9c33955d065ef6bc3eaf84f9e298
LiaoZiyang <lzy08238013@163.com>
2025-04-23 10:03:49 +0800

    多线程-学习，练习 插入线程练习01

---
+++ src/practice/multi_threading/test/test04/Test02.java
@@ -0,0 +1,35 @@
+package practice.multi_threading.test.test04;
+
+/**
+ * ClassName: Test02
+ * Package: practice.multi_threading.test.test04
+ * Description:
+ *
+ * @author 廖子杨
+ * CreateTime: 2025/4/23 - 09:59
+ * Version - v 1.0
+ */
+public class Test02 {
+    public static void main(String[] args) {
+        MyThreads m1 = new MyThreads();
+        m1.start();
+        try {
+            m1.join();
+        }catch (InterruptedException e ) {
+            e.printStackTrace();
+        }
+
+        for (int i = 0; i < 10; i++) {
+            System.out.println(Thread.currentThread().getName() + "  " + i);
+        }
+    }
+}
+
+class MyThreads extends Thread{
+    @Override
+    public void run() {
+        for (int i = 0; i < 10; i++) {
+            System.out.println(Thread.currentThread().getName() + "  " + i);
+        }
+    }
+}
