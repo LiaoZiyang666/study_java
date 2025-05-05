8a7e1401ea680c74d9a75d48cf4bf3d731a414e3
LiaoZiyang <lzy08238013@163.com>
2025-05-05 23:18:50 +0800

    test

---
+++ src/practice/multi_threading/test/test01/Hello.java
@@ -0,0 +1,16 @@
+package practice.multi_threading.test.test01;
+
+/**
+ * ClassName: Hello
+ * Package: practice.multi_threading.test.test01
+ * Description:
+ *
+ * @author 廖子杨
+ * CreateTime: 2025/5/5 - 23:18
+ * Version - v 1.0
+ */
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
